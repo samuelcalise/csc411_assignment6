20d862506b94b84e492fe6b72bd7608df8d8fc99
samuelcalise <113198869+samuelcalise@users.noreply.github.com>
2023-12-07 16:07:47 +0000

    need to test initial time

---
+++ rum/target/profiling/deps/rum-6e7d93271d07f1e7.d
@@ -0,0 +1,9 @@
+/workspaces/csc411_assignment6/rum/target/profiling/deps/rum-6e7d93271d07f1e7: src/main.rs src/rum.rs src/segment.rs src/register.rs src/um_instruction.rs
+
+/workspaces/csc411_assignment6/rum/target/profiling/deps/rum-6e7d93271d07f1e7.d: src/main.rs src/rum.rs src/segment.rs src/register.rs src/um_instruction.rs
+
+src/main.rs:
+src/rum.rs:
+src/segment.rs:
+src/register.rs:
+src/um_instruction.rs:
